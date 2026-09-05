74b9d7424a09101f6ae3180f1616d5605e896121
Willlimaneto <awilllima@byupathway.edu>
2026-09-06 00:46:59 +0100

    Commited all the changes

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.textContent = today.getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
