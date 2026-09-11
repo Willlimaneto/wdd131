f24aa5f6225074f5820b7e22f45b446d50665f6a
Willlimaneto <awilllima@byupathway.edu>
2026-09-11 01:26:56 +0100

    Completed javascript debugging activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,27 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
-const li = document.createElement('li');
-const deletedButton = document.createElement('button');
-li.textContent = input.value;
- deletedButton.textContent = '❌';
- li.append('deletedButton');
- list.append('li');
+
+button.addEventListener('click', function () {
+
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = '❌';
+
+        deleteBtn.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+
+        input.value = '';
+    }
+
+    input.focus();
+});
