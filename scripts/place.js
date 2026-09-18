f23bd94ce3484768d858f32fc99646216eb62ec0
Willlimaneto <awilllima@byupathway.edu>
2026-09-18 23:22:50 +0100

    Fix place.js issues

--- scripts/place.js
+++ scripts/place.js
@@ -1,9 +1,8 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
 
-// Valores estáticos (por enquanto)
-const temperature = 27; // °C
-const windSpeed = 10;   // km/h
+const temperature = 27; 
+const windSpeed = 10; 
 
 function calculateWindChill(temp, wind) {
 	return (13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16)).toFixed(1);
